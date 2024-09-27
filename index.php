186ed29b01ed5e8a171198c210f1adeeaf172620
projeto-miguel <miguel2020carneiro@gmail.com>
2024-09-26 22:21:54 -0300

    continuando lógica de url

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
+
+use Api\Core\Core;
+use Api\Router\Router;
+
 require_once __DIR__ . "/vendor/autoload.php";
 require_once __DIR__ . "/src/core/main.php";
 
 // PEGANDO CREDENCIAIS DO BANCO DE DADOS PELO ARQUIVO .ini
 define("_CONF",parse_ini_file(__DIR__ . "/src/.ini",true));
 
+Core::load(Router::routes());
--- src/core/Core.php
+++ src/core/Core.php
@@ -1,9 +1,23 @@
 <?php
-
-namespace Api;
+namespace Api\Core;
 
 class Core{
-    public static function load(): void{
-        print_r($_GET);
+    public static function load(array $routes): void{
+        $url = isset($_GET['url']) ? '/' . $_GET['url'] . '/' : '/';
+
+        $controllerPrefix = "Api\\Controllers\\";
+
+        foreach ($routes as $route) {
+            $pattern = '#^' . preg_replace('/\{id\}/', '([\w-]+)', $route['url']) . '$#';
+
+            if(preg_match($pattern, $url, $matches)){
+                array_shift($matches);
+
+                [$controller ,$action] = explode('@', $route['action']);
+                $controller = $controllerPrefix . $controller;
+                $extendController = new $controller();
+                $extendController->$action();
+            }
+        }
     }
 }
